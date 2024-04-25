d5bc249c28697835be66085b67116f2e394538d8
Leandro Torrez <lean170899@gmail.com>
2024-04-25 00:09:19 -0300

    sprint 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './component/home/home.component';
+import { LoginComponent } from './component/login/login.component';
+import { QuienSoyComponent } from './component/quien-soy/quien-soy.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:"",
+        component:HomeComponent
+    },
+    {
+        path:"login",
+        component:LoginComponent
+    },
+    {
+        path:"home",
+        component:HomeComponent
+    },
+    {
+        path:"quien-soy",
+        component:QuienSoyComponent
+    },
+];
